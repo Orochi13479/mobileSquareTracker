6565b95f82a5855b69836895e566aa5911ad1609
Matthew <matthew13479@gmail.com>
2023-09-27 23:56:34 +1000

    ROS is reading camera and all data from turtlebot simulation

--- imageProcessing.m
+++ imageProcessing.m
@@ -1,5 +1,14 @@
-clc;
-clear all;
-close all;
+% clc;
+% clear all;
+% close all;
 rosshutdown;
-rosinit;
+% rosinit;
+ip = "http://192.168.211.137:11311";
+rosinit(ip)
+clear('pub','sub','node')
+node = ros.Node('/driving');
+rgb = rossubscriber('/camera/rgb/image_raw'); %
+depth = rossubscriber('/camera/depth/image_raw'); %
+odom = rossubscriber('/odom');
+drive = ros.Publisher(node,'/cmd_vel','geometry_msgs/Twist');
+msg = rosmessage('geometry_msgs/Twist');
