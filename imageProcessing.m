37f5a180e23840cddb0191fb1da399f1727f80df
daniell-lo <daniel.lo1706@gmail.com>
2023-09-28 16:29:50 +1000

    Altered matlab

--- imageProcessing.m
+++ imageProcessing.m
@@ -5,7 +5,9 @@ close all;
 
 % Shutdown ROS and Relaunch using ROS_MASTER_URI AND ROS_IP
 rosshutdown;
-ip = "http://192.168.211.137:11311";
+%ip = "http://192.168.211.137:11311";
+ip = "http://192.168.19.132:11311"; %Daniel IP
+
 rosinit(ip)
 
 % Setting up node publisher
