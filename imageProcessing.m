b85623ce81cf4a7a7b0659115dd76bd891e894d3
Matthew <matthew13479@gmail.com>
2023-09-28 11:42:26 +1000

    Changed to waffle from waffle pi

--- imageProcessing.m
+++ imageProcessing.m
@@ -1,11 +1,10 @@
-% clc;
-% clear all;
-% close all;
+clc;
+clear all;
+close all;
 rosshutdown;
-% rosinit;
 ip = "http://192.168.211.137:11311";
 rosinit(ip)
-clear('pub','sub','node')
+
 node = ros.Node('/driving');
 rgb = rossubscriber('/camera/rgb/image_raw'); %
 depth = rossubscriber('/camera/depth/image_raw'); %
