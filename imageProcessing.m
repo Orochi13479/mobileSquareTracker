c9c7510539c49253e312f0f025f043aa21732528
Matthew <matthew13479@gmail.com>
2023-10-18 10:26:53 +1100

    Cleaning file and timing algos

--- imageProcessing.m
+++ imageProcessing.m
@@ -10,7 +10,7 @@ rosinit();
 
 % Setting up node publisher
 node = ros.Node('/driving');
-drive = ros.Publisher(node,'/cmd_vel','geometry_msgs/Twist');
+drive = ros.Publisher(node, '/cmd_vel', 'geometry_msgs/Twist');
 msg = rosmessage('geometry_msgs/Twist');
 
 % Setting up all necessary subscribers
@@ -24,41 +24,36 @@ squarePattern = rgb2gray(imread('Initial_image.png'));
 %% Operation
 while true
     disp("Running...")
+
     % Pull Rotation and transformation matrices
-    turtleTF = trvec2tform([odom.LatestMessage.Pose.Pose.Position.X odom.LatestMessage.Pose.Pose.Position.Y odom.LatestMessage.Pose.Pose.Position.Z]);
-    turtleQuat = quaternion([odom.LatestMessage.Pose.Pose.Orientation.W odom.LatestMessage.Pose.Pose.Orientation.X odom.LatestMessage.Pose.Pose.Orientation.Y odom.LatestMessage.Pose.Pose.Orientation.Z]);
+    turtleTF = trvec2tform([odom.LatestMessage.Pose.Pose.Position.X, odom.LatestMessage.Pose.Pose.Position.Y, odom.LatestMessage.Pose.Pose.Position.Z]);
+    turtleQuat = quaternion([odom.LatestMessage.Pose.Pose.Orientation.W, odom.LatestMessage.Pose.Pose.Orientation.X, odom.LatestMessage.Pose.Pose.Orientation.Y, odom.LatestMessage.Pose.Pose.Orientation.Z]);
     rotMatrix = rotm2tform(rotmat(turtleQuat, 'point'));
 
     % Pull Image and Depth data
-    [rgbData,alpha]=readImage(rgb.LatestMessage);
+    [rgbData, ~] = readImage(rgb.LatestMessage);
     gsData = rgb2gray(rgbData);
-    [depthData,alpha]=readImage(depth.LatestMessage);
-    
-    % Feature Detection
-    ptsPattern = detectKAZEFeatures(squarePattern);
-    ptsData = detectKAZEFeatures(gsData);
-    [featurePattern,validPtsPattern] = extractFeatures(squarePattern,ptsPattern);
-    [featureData,validPtsData] = extractFeatures(gsData,ptsData);
+    [depthData, ~] = readImage(depth.LatestMessage);
 
     % Feature Detection
-    % ptsPattern = detectSURFFeatures(squarePattern);
-    % ptsData = detectSURFFeatures(gsData);
-    % [featurePattern,validPtsPattern] = extractFeatures(squarePattern,ptsPattern);
-    % [featureData,validPtsData] = extractFeatures(gsData,ptsData);
-    
+    tic
+    ptsPattern = detectSURFFeatures(squarePattern);
+    ptsData = detectSURFFeatures(gsData);
+    [featurePattern, validPtsPattern] = extractFeatures(squarePattern, ptsPattern);
+    [featureData, validPtsData] = extractFeatures(gsData, ptsData);
+
     % Match pairs and display them
-    indexPairs = matchFeatures(featurePattern,featureData);
-    matchedPattern = validPtsPattern(indexPairs(:,1));
-    matchedData = validPtsData(indexPairs(:,2));
-    
-    showMatchedFeatures(squarePattern, gsData,matchedPattern ,matchedData)
+    indexPairs = matchFeatures(featurePattern, featureData);
+    matchedPattern = validPtsPattern(indexPairs(:, 1));
+    matchedData = validPtsData(indexPairs(:, 2));
+    % disp(toc)
+    showMatchedFeatures(squarePattern, gsData, matchedPattern, matchedData)
+    % disp(toc)
+
+
+    size(indexPairs, 1)
+
 
     % Delay
     pause(1);
 end
-
-
-
-
-
-
