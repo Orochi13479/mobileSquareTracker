8440622dc934e5d80a5126731f092f9809a5da4b
Matthew <matthew13479@gmail.com>
2023-10-19 17:49:06 +1100

    Frames and computing depths for basee frame

--- imageProcessing.m
+++ imageProcessing.m
@@ -21,16 +21,14 @@ odom = rossubscriber('/odom');
 % Reading Square pattern
 squarePattern = rgb2gray(imread('Initial_image.png'));
 
-
 % Pause to wait for ROS
 pause(2);
 
 %% Operation
 while true
     disp("Running...")
-    
-    [controlInput,averageDepth,orientationError] = dataProcessing(rgb,depth,odom,squarePattern);
-    
+
+    [controlInput, averageDepth, orientationError] = dataProcessing(rgb, depth, odom, squarePattern);
 
     % Adjust the robot's movement
     if abs(controlInput) >= 0.01
@@ -40,11 +38,10 @@ while true
         msg.Angular.Z = 0;
         disp("Stopped")
     end
-    
-    
+
     % Publish control commands
     send(drive, msg);
-    
+
     % Display depth and orientation error
     disp(['controlInput: ', num2str(controlInput)]);
     % disp(['Average Depth: ', num2str(averageDepth)]);
@@ -54,10 +51,21 @@ while true
     pause(1);
 end
 
-function [controlInput,averageDepth,orientationError]  = dataProcessing(rgb,depth,odom,squarePattern)
+function [controlInput, averageDepth, orientationError, perpendicularLine] = dataProcessing(rgb, depth, odom, squarePattern)
     % Define target orientation (perpendicular to the pattern)
-    targetOrientation = 0; % Adjust as needed
+    targetOrientation = deg2rad(90); % Adjust as needed
     previousError = 0;
+    
+    % Different Frame poses relative each other copied from model file
+    base2image_joint = [0.064 -0.065 0.094]; % Base to image_joint
+    base2camera = [0.064 -0.047 0.107]; % Base to Camera
+    camera2optical = [-90 90 0]; % Rotation only Optical frame rotation
+    image_joint2camera = base2camera - base2image_joint; % image_joint to camera
+    base2optical = trvec2tform(base2image_joint) * trvec2tform(image_joint2camera) * eul2tform(deg2rad(camera2optical), "XYZ"); % Base to optical frame
+    
+    % Camera calibration and distortion parameters
+    K = [1206.89, 0.0, 960.5, 0.0, 1206.89, 540.5, 0.0, 0.0, 1.0]; % rostopic echocamera/rgb/camera_info 
+
 
     % Define control parameters (adjust as needed)
     Kp = 0.1; % Proportional gain
@@ -67,6 +75,7 @@ function [controlInput,averageDepth,orientationError]  = dataProcessing(rgb,dept
     turtleTF = trvec2tform([odom.LatestMessage.Pose.Pose.Position.X, odom.LatestMessage.Pose.Pose.Position.Y, odom.LatestMessage.Pose.Pose.Position.Z]);
     turtleQuat = quaternion([odom.LatestMessage.Pose.Pose.Orientation.W, odom.LatestMessage.Pose.Pose.Orientation.X, odom.LatestMessage.Pose.Pose.Orientation.Y, odom.LatestMessage.Pose.Pose.Orientation.Z]);
     rotMatrix = rotm2tform(rotmat(turtleQuat, 'point'));
+    turtleTF2World = turtleTF * rotMatrix;
 
     % Pull Image and Depth data
     [rgbData, ~] = readImage(rgb.LatestMessage);
@@ -83,17 +92,36 @@ function [controlInput,averageDepth,orientationError]  = dataProcessing(rgb,dept
     indexPairs = matchFeatures(featurePattern, featureData);
     matchedPattern = validPtsPattern(indexPairs(:, 1));
     matchedData = validPtsData(indexPairs(:, 2));
-    
-    % Remove outliers
-    [~,inlierData,inlierPattern] = estimateGeometricTransform(matchedData, matchedPattern,'similarity');
 
-    % For every valid match, obtain the depth data
-    depthPts = zeros(1, inlierData.Count);
-    
+    % Remove outliers
+    [~, inlierData, inlierPattern] = estimateGeometricTransform(matchedData, matchedPattern, 'similarity');
+
+    % Preallocate arrays for efficiency
+    numFeatures = inlierData.Count;
+    depthPts = zeros(1, numFeatures);
+    features2base = nan(3, numFeatures);
+        
+    % Match depth data with feature pts and transform to base frame for
+    % later use
     for i = 1:inlierData.Count
-        depthPts(i) = depthData(round(inlierData.Location(i, 2)), round(inlierData.Location(i, 1)));
+        % Extract feature location indices
+        featureRow = round(inlierData.Location(i, 2));
+        featureCol = round(inlierData.Location(i, 1));
+        
+        % Extract depth from the depth image
+        depthPts(i) = depthData(featureRow, featureCol);
+        
+        % Calculate the camera coordinates of the feature
+        featurePts = [depthPts(i) * featureCol; depthPts(i) * featureRow; depthPts(i)];
+        % Features in the camera frame
+        features2camera = K \ featurePts;
+        
+        % Extend the 3D point to homogeneous coordinates and transform to the base frame
+        homogeneousCoords = [features2camera; 1];
+        features2base(:, i) = base2optical * homogeneousCoords;
     end
-    
+
+
     % Calculate the average depth
     averageDepth = mean(depthPts);
 
@@ -101,10 +129,14 @@ function [controlInput,averageDepth,orientationError]  = dataProcessing(rgb,dept
     currentOrientation = atan2(rotMatrix(2, 1), rotMatrix(1, 1));
     orientationError = targetOrientation - currentOrientation;
 
+    
+
     % Apply a PID controller to adjust the robot's movement
     controlInput = Kp * orientationError + Kd * (orientationError - previousError);
 
     % Update previous error
     previousError = orientationError;
 end
+
+
 
